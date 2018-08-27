eb51d8828fa109a38b090951d0453ecdb7f86753
Andrew Mason <andrewmasonmedia@gmail.com>
2018-08-27 16:04:19 -0400

    Updates to nav --> added community page, updates to footer --> copied over parallax additions, updates to legal page banner, updates to about page banner --> made svg header

--- js/main.js
+++ js/main.js
@@ -279,7 +279,8 @@ $(".content-tween").each(function() {
 
 	var scene4 = new ScrollMagic.Scene({
 	        triggerElement: this,
-            triggerHook: '0.8',
+//            triggerHook: '0.8',
+            triggerHook: 'onEnter',
 	        offset: -100,
 			reverse:true
 	    })
