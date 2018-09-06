bd30225d7d1f5226b82d3b4e31caf4bfc13ff8c2
Andrew Mason <andrewmasonmedia@gmail.com>
2018-09-06 17:45:26 -0400

    Updates to security page copy and layout, removed on of the col sections

--- security.php
+++ security.php
@@ -13,35 +13,33 @@ include 'includes/header.php';
             <img class="security-shield mt-5 pt-5 mb-3 rellax" src="/img/jaxx-shield-orange.png" alt="Jaxx Liberty Security" data-rellax-speed="-2">
             <!--<h1 class="site-title relative z-1 pb-2">Your Security is Our Mission.</h1>-->
             <h1 class="site-title relative z-1 x-bold m-0 mb-1">
+                <span class="d-inline-block">We</span>
+                <span class="d-inline-block">Value</span>
                 <span class="d-inline-block">Your</span>
                 <span class="d-inline-block">Security</span>
-                <span class="d-inline-block">is</span>
-                <span class="d-inline-block">Our</span>
-                <span class="d-inline-block">Mission</span>
             </h1>
             <hr class="w-25 bg-white">
             <h2 class="site-sub-title relative z-1 pt-1 pb-3">
-                <span class="d-inline-block">Jaxx</span>
-                <span class="d-inline-block">Liberty</span>
-                <span class="d-inline-block">is</span>
-                <span class="d-inline-block">the</span>
-                <span class="d-inline-block">most</span>
-                <span class="d-inline-block">secure</span>
-                <span class="d-inline-block">cryptocurrency</span>
-                <span class="d-inline-block">wallet</span>
-                <span class="d-inline-block">available</span>
+                <span class="d-inline-block">Explore</span>
+                <span class="d-inline-block">our</span>
+                <span class="d-inline-block">new</span>
+                <span class="d-inline-block">and</span>
+                <span class="d-inline-block">improved</span>
+                <span class="d-inline-block">security</span>
+                <span class="d-inline-block">model</span>
             </h2>
         </div>
         
         <!--container-->
         <div class="container-fluid p-0">
-        
+           
             <!--row 1 security intro-->
             <div id="sec-features" class="row d-flex bg-img-banner-security bg-dots p-4 shadow-lg content-tween">
                 <div class="col-lg-12 d-flex flex-column justify-content-center align-items-center text-center min-70 p-3 rellax" data-rellax-speed="-1">
                     <div class="wrap d-flex flex-column justify-content-center align-items-center bg-white-op p-5 w-100 h-100 border rellax" data-rellax-speed="3">
-                        <h2 class="section-title x-bold relative z-1 m-0 content-stagger-up">More than a million users trust Jaxx.</h2>
-                        <p class="relative z-1 py-3 content-stagger-up">We take security seriously. From industry-standard encryption to key privacy and rigorous code review, our highest priority is ensuring the safety and privacy of our users. Explore some of the new and improved security features found in Jaxx Liberty below.</p>
+                        <h2 class="section-title x-bold relative z-1 m-0 content-stagger-up">Millions of Users Already Trust Jaxx.</h2>
+                        <p class="font-weight-bold relative z-1 py-3 content-stagger-up">We take security seriously. Jaxx Liberty does not require users to provide personal information to interact with their digital assets. Explore enhanced security features found in Jaxx Liberty below.</p>
+                        <i class="fa fa-angle-double-down p-0 display-4"></i>
                     <!--<img class="img-shield content-stagger-up" src="/img/jaxx-shield.png" alt="Jaxx Liberty Security">-->
                     </div>
                     <img class="img-shield absolute z-0 fade-in-slow" src="/img/Sheild_Navy.png" alt="Jaxx Liberty Security">
@@ -49,54 +47,52 @@ include 'includes/header.php';
             </div><!--end row-->
 
            <!--row 2 security features-->
-            <div class="row d-flex bg-img-banner-security relative z-1 content-tween">
-                <div class="col-xl-4 col-md-6 d-flex flex-column justify-content-start align-items-start min-50 bg-orange text-light p-5 rellax">
-                    <div class="wrap p-4 border border-light min-50 content-stagger-up">
-                        <img class="jaxx-navbar-brand content-stagger-up" src="/img/Jaxx_Icon_Reversed_Large_RGB_Screen.png" alt="Jaxx Liberty Security">
-                        <h2 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 content-stagger-up">Private Keys</h2>
-                        <p class="border-top border-light py-3 content-stagger-up">Like the legacy version of Jaxx wallet, the user’s secrets (mnemonic/private keys) are stored on the client side instead of the server side, therefore they are not exposed to anyone, even Decentral employees.</p>
-                    </div>
-                </div>
-                                
-                <div class="col-xl-4 col-md-6 d-flex flex-column justify-content-start align-items-start min-50 bg-white-op p-5">
+            <div class="row d-flex bg-img-banner-security relative z-1 content-tween o-2">
+                <div class="col-lg-6 d-flex flex-column justify-content-start align-items-start min-50 bg-white-op p-5">
                     <div class="wrap p-4 border border-dark min-50 content-stagger-up">
                         <img class="jaxx-navbar-brand content-stagger-up" src="/img/Jaxx_Icon_Reversed_Large_RGB_Screen.png" alt="Jaxx Liberty Security">
-                        <h2 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 content-stagger-up">Encryption</h2>
-                        <p class="border-top border-dark py-3 content-stagger-up">Once a password is set it is encrypted and stored securely using industry-standard encryption procedure. They can only be decrypted with the password known by the user.  Liberty does not store the user-password in the system.</p>
+                        <h2 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 mb-4 relative z-1 content-stagger-up">Encryption</h2>
+                        <p class="border-top border-dark py-3 content-stagger-up">Once a password is set, data is stored securely with an encryption procedure. It can only be decrypted with the password known by the user. Jaxx Liberty does not store the user-password in the system.</p>
                     </div>
                 </div>
-                             
+                
+                <div class="col-lg-6 d-flex flex-column justify-content-start align-items-start min-50 bg-orange text-light p-5 o-3 rellax">
+                    <div class="wrap p-4 border border-light min-50 content-stagger-up">
+                        <img class="jaxx-navbar-brand content-stagger-up" src="/img/Jaxx_Icon_Reversed_Large_RGB_Screen.png" alt="Jaxx Liberty Security">
+                        <h2 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 mb-4 relative z-1 content-stagger-up">Private Keys</h2>
+                        <p class="border-top border-light py-3 content-stagger-up">As with the previous version of the Jaxx wallet, users' secret 12-word phrase is stored and, therefore, not exposed to anyone, even Decentral employees.</p>
+                    </div>
+                </div>
 <!--
                 <div class="col-xl-4 col-md-6 d-flex flex-column justify-content-start align-items-start min-50 bg-orange text-light p-5 rellax">
                     <div class="wrap p-4 border border-light min-50 content-stagger-up">
                         <img class="jaxx-navbar-brand content-stagger-up" src="/img/Jaxx_Icon_Reversed_Large_RGB_Screen.png" alt="Jaxx Liberty Security">
-                        <h2 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 content-stagger-up">Address Integrity</h2>
-                        <p class="border-top border-light py-3 content-stagger-up">Wallet generated addresses are periodically checked in terms of integrity to reduce risk of malicious modification of addresses.</p>
+                        <h2 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 content-stagger-up">New Model</h2>
+                        <p class="border-top border-light py-3 content-stagger-up">Our new and improved security model in Jaxx Liberty offers the option to encrypt sensitive information with a strong alphanumeric password.  </p>
                     </div>
                 </div>
 -->
-                
-               <div class="col-xl-4 col-md-6 d-flex flex-column justify-content-start align-items-start min-50 bg-white-op p-5">
-                    <div class="wrap p-4 border border-dark min-50 content-stagger-up">
+                <div class="col-lg-6 d-flex flex-column justify-content-start align-items-start text-light bg-orange p-5 rellax">
+                    <div class="wrap w-100 p-4 border border-light min-50 content-stagger-up">
                         <img class="jaxx-navbar-brand content-stagger-up" src="/img/Jaxx_Icon_Reversed_Large_RGB_Screen.png" alt="Jaxx Liberty Security">
-                        <h2 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 content-stagger-up">Ownership & Control</h2>
-                        <p class="border-top border-dark py-3 content-stagger-up">Jaxx uses a 12-word “Masterseed” to help you manage the private keys to your Bitcoin, Ethereum, and other blockchain assets.</p>
-                   </div>
-                </div>
-                               
-                <div class="col-xl-4 col-md-6 d-flex flex-column justify-content-start align-items-start text-light bg-orange p-5 rellax">
-                    <div class="wrap p-4 border border-light min-50 content-stagger-up">
-                        <img class="jaxx-navbar-brand content-stagger-up" src="/img/Jaxx_Icon_Reversed_Large_RGB_Screen.png" alt="Jaxx Liberty Security">
-                        <h2 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 content-stagger-up">Code Review</h2>
-                        <p class="border-top border-light py-3 content-stagger-up">Rigid code review processes minimizes security vulnerabilities.</p>
+                        <h2 class="font-weight-bold p-3 bg-navy orange relative z-1 skew shadow-lg py-2 mb-4 content-stagger-up">Code Review</h2>
+                        <p class="border-top border-light py-3 content-stagger-up">State-of-the-art static and dynamic analysis tools minimize security vulnerabilities. Code signing prevents the alteration and corruption of code, while checksums further ensure the integrity of data during transmission.</p>
                     </div>
                 </div>
-                                
-                <div class="col-xl-4 col-md-6 d-flex flex-column justify-content-start align-items-start bg-white-op p-5">
+                
+               <div class="col-lg-6 d-flex flex-column justify-content-start align-items-start min-50 bg-white-op p-5 o-4">
                     <div class="wrap p-4 border border-dark min-50 content-stagger-up">
                         <img class="jaxx-navbar-brand content-stagger-up" src="/img/Jaxx_Icon_Reversed_Large_RGB_Screen.png" alt="Jaxx Liberty Security">
-                        <h2 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 content-stagger-up">Secure across platforms</h2>
-                        <p class="border-top border-dark py-3 content-stagger-up">Use your Masterseed to view your wallet on iOS, Android, Chrome Extension, Windows, Mac and Linux desktops.</p>
+                        <h2 class="font-weight-bold p-3 bg-navy orange relative z-1 skew shadow-lg py-2 mb-4 content-stagger-up">Ownership & Control</h2>
+                        <p class="border-top border-dark py-3 content-stagger-up">Jaxx uses a 12-word phrase to help you manage the private keys to your Bitcoin, Ethereum and other digital assets.</p>
+                   </div>
+                </div>
+                                
+                <div class="col-lg-12 d-flex flex-column justify-content-start align-items-start bg-white-op p-5">
+                    <div class="wrap w-100 p-4 border border-dark min-50 content-stagger-up">
+                        <img class="jaxx-navbar-brand content-stagger-up" src="/img/Jaxx_Icon_Reversed_Large_RGB_Screen.png" alt="Jaxx Liberty Security">
+                        <h2 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 mb-4 content-stagger-up">Secure across platforms</h2>
+                        <p class="border-top border-dark py-3 content-stagger-up">Use your 12-word phrase to view your wallet on iOS, Android, Chrome Extension, Windows, Mac and Linux systems.</p>
                     </div>
                 </div>
             </div><!--end row-->
@@ -105,8 +101,8 @@ include 'includes/header.php';
             <div class="row d-flex bg-white min-100 content-tween rellax">
                 <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left bg-dots min-50 p-5 o-12 rellax">
                     <img class="jaxx-navbar-brand content-stagger-up" src="/img/Jaxx_Primary_Icon_RGB_Screeen.svg" alt="Jaxx Liberty Security">
-                    <h3 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 content-stagger-up">Jaxx never has access to your funds.</h3>
-                    <p class="border-top py-3 content-stagger-up">They remain securely in your control on their respective blockchains. This means that if you back-up your Masterseed you will be easily able to recover your wallets if you lose access to the device they were on.</p>
+                    <h3 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 mb-4 relative z-1 content-stagger-up">We never access your digital assets</h3>
+                    <p class="border-top py-3 content-stagger-up">Manage your own digital holdings. And by backing up your 12-word phrase, you can recover your wallets should you lose access to the device containing them.</p>
                 </div><!--end col-->
                 <div class="col-lg-6 bg-img-hand-device p-0"></div>
             </div><!--end row-->
@@ -116,16 +112,16 @@ include 'includes/header.php';
                 <div class="col-lg-6 bg-img-people-1 p-0"></div>
                 <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left bg-dots min-50 p-5 rellax">
                     <img class="jaxx-navbar-brand content-stagger-up" src="/img/Jaxx_Primary_Icon_RGB_Screeen.svg" alt="Jaxx Liberty Security">
-                    <h3 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 content-stagger-up">No personal information required.</h3>
-                    <p class="border-top py-3 content-stagger-up">Users are not required to provide personal information to interact with their digital assets. We care about safeguarding your digital assets and protecting your identity.</p>
+                    <h3 class="font-weight-bold p-3 bg-navy orange skew shadow-lg py-2 content-stagger-up">No personal information required</h3>
+                    <p class="border-top py-3 content-stagger-up">You are not required to provide your personal information to interact with your digital assets. We care about safeguarding your digital value while protecting your identity.</p>
                 </div><!--end col-->
             </div><!--end row-->           
 
            <!-- row 5 download reminder CTA-->
            <div class="row bg-orange text-white min-70 content-tween">
-               <div class="col-sm-12 d-flex flex-column justify-content-center align-items-center text-center bg-download-gradient p-4 parallelo">
+               <div class="col-sm-12 d-flex flex-column justify-content-center align-items-center text-center bg-download-gradient p-4 parallelo">x
                    <img class="jaxx-logo-text content-stagger-up" src="/img/JaxxLiberty_Logo_RGB_Screen%20(white%20Liberty%20white%20X).svg" alt="">
-                   <h4 class="font-weight-bold w-75 pb-3 pt-2 content-stagger-up">Join more than a million others using Jaxx.</h4>
+                   <h4 class="font-weight-bold w-75 pb-3 pt-2 content-stagger-up">Jaxx. Used by millions. Join now.</h4>
                    <div class="content-stagger-up">
                        <a href="/downloads.php" class="btn btn-lg btn-orange animsition-link" data-animsition-out-class="fade-out-down-sm" data-animsition-out-duration="800">Download</a>
                    </div>
