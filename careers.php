c57bfdcae238ed00c88bfad4a089f3782f7edba5
Andrew Mason <andrewmasonmedia@gmail.com>
2018-09-07 09:49:27 -0400

    Updates to careers page -- removed image divs; update to index first section buttons animation out styles

--- careers.php
+++ careers.php
@@ -33,20 +33,15 @@ include 'includes/header.php';
         <div id="careers" class="container-fluid bg-white p-0">
             <div class="row d-flex px-5 content-tween">
                 <div class="col-lg-12 d-flex flex-column justify-content-center align-items-start text-left min-50 py-5 px-3">
-                    <h2 class="section-title font-weight-bold content-stagger-up">Join Canada's Next Unicorn</h2>
-                    <p class="pt-3 border-top content-stagger-up">Decentral is leading the blockchain movement in Canada and around the world. We create the Jaxx Blockchain Interface - a cryptocurrency wallet used today by more than 700,000 users. We are interested in meeting talented and motivated people in the Toronto area. Our business is growing rapidly and we're actively seeking a variety of candidates for the roles posted below. We are primarily focused on our Jaxx product, the multi-token blockchain interface that provides a unified experience across 6 platforms and devices. Jaxx enables crypto-to-crypto buying and selling with frictionless in-wallet conversion. Users are always in control of their keys and Jaxx neither holds nor has access to customer funds. Driven by design and user experience and built with simplicity in mind Jaxx’s mission is to become the interface to the blockchain world. Download and experience Jaxx through jaxx.io</p>
-                    <p class="pb-3 border-bottom content-stagger-up">If you don’t see a role that fits your skill set but you would like to come work for the largest blockchain startup in Canada, send your resume to us at <a href="mailto:careers@decentral.ca?subject=Applying for a Position" class="orange text-bold">careers@decentral.ca</a>. We are a team who take big risks, who celebrate our journey and know we are part of a movement that is changing the way the world thinks and acts about the exchange of value and currency.</p>
+                    <h2 class="section-title font-weight-bold content-stagger-up">We inform. We empower. We unite.</h2>
+                    <p class="pt-3 border-top content-stagger-up">Decentral is leading the blockchain movement in Canada and around the world, changing the way we think about the exchange of value. We created the multi-token Jaxx blockchain interface — a digital asset wallet that has touched the lives of millions globally. We are keen to meet talented and motivated people to join us in our Toronto offices. Our business is growing rapidly, and we are actively seeking a variety of candidates for the roles posted below.</p>
+                    <p class="pb-3 border-bottom content-stagger-up">If you do not see a role that fits your skillset but would like to be a part of one of Canada’s leading blockchain companies, send us your resume at <a href="mailto:careers@decentral.ca?subject=Applying for a Position" class="orange text-bold">careers@decentral.ca</a>.</p>
                 </div>
             </div>
 
-           <!--divider-->
-            <div class="row d-flex p-0">
-                <div class="col-sm-12 p-5 bg-img-office rellax" data-rellax-speed="-2"></div>
-            </div>
-            
             <!--position 1-->
             <div class="row d-flex py-4 bg-dots content-tween">
-                <div class="col-lg-12 d-flex flex-column justify-content-center align-items-end text-right min-50 p-5">
+                <div class="col-lg-12 d-flex flex-column justify-content-center align-items-end text-right min-50 p-5 rellax">
                     <h3 class="site-sub-title font-weight-bold bg-navy orange shadow-lg rounded p-3 mb-3 content-stagger-up">Angular Developer</h3>
                     <h5 class="content-stagger-up">Full Time - Toronto, Ontario</h5>
                     <p class="pt-3 border-top content-stagger-up">We seek a Angular Developer to take us through the second phase of our journey. You will be pivotal in developing, implementing, and optimizing a new product Decentral is bringing to market. This product will be used by millions around the globe and we will be counting on you to launch brand new features, create really cool visual elements, work with backend APIs, all while keeping customer security top of mind.</p>
@@ -54,16 +49,11 @@ include 'includes/header.php';
                     <p class="pb-3 border-bottom content-stagger-up">As part of the interview process, we will ask you to provide a portfolio to showcase your past work.. We are excited for you to show your accomplishments and will use this to facilitate the communication during the interview.</p>
                     <a href="mailto:careers@decentral.ca?subject=Applying to Position: Angular Developer" class="btn btn-lg btn-orange my-3 content-stagger-up" target="_blank">Apply Now</a>
                 </div>
-            </div>          
-            
-            <!--divider-->
-            <div class="row d-flex">
-                <div class="col-sm-12 p-5 bg-img-office rellax" data-rellax-speed="-2"></div>
             </div>
 
             <!--position 2-->
             <div class="row d-flex py-4 bg-dots content-tween">
-                <div class="col-lg-12 d-flex flex-column justify-content-center align-items-start text-left p-5">
+                <div class="col-lg-12 d-flex flex-column justify-content-center align-items-start text-left p-5 rellax">
                     <h3 class="site-sub-title font-weight-bold bg-navy orange shadow-lg rounded p-3 mb-3 content-stagger-up">SEO Specialist</h3>
                     <h5 class="content-stagger-up">Full Time - Toronto, Ontario</h5>
                     <p class="pt-3 border-top content-stagger-up">Our mission includes providing world-class content and education in an unbiased and easily-understood fashion. The goal is to become the most trustworthy source in the blockchain, cryptocurrency and financial technology space. The challenge is to build out SEO strategy in a meaningful, purposeful way that is true to our brand and values--including a pro-privacy, low-friction experience that respects our users and our readers. Are you interested in creating a new path for SEO? If so, this role is for you.</p>
@@ -89,14 +79,10 @@ include 'includes/header.php';
                     </div>
                 </div>
             </div><!--end row-->
-            
-            <div class="row d-flex">
-                <div class="col-sm-12 p-5 bg-img-office rellax" data-rellax-speed="-2"></div>
-            </div>
 
             <!--about decentral-->
            <div class="row d-flex py-4 bg-dots content-tween">
-                <div class="col-lg-12 d-flex flex-column justify-content-center align-items-end text-right bg-dots p-5">
+                <div class="col-lg-12 d-flex flex-column justify-content-center align-items-end text-right bg-dots p-5 rellax">
                     <h2 class="site-sub-title font-weight-bold bg-navy orange shadow-lg rounded p-3 mb-3 content-stagger-up">About Decentral</h2>
                     <p class="pt-3 border-top content-stagger-up">Located in the heart of downtown Toronto and home to hundreds of blockchain community events, Decentral spawned Ethereum. Decentral’s strategic approach means Jaxx Liberty neither holds nor has access to customer funds. Driven by design and user experience and built with simplicity in mind, Jaxx’s mission is to become the interface to the blockchain world. Learn more about Decentral Project at <a href="https://decentralproject.org/" target="_blank">decentralproject.org</a>.</p>
                     <p class="content-stagger-up">We engage in inclusive hiring. We encourage people from all backgrounds, cultures, life experiences, etc., to apply. Decentral serves catered food, which includes vegan options, at company lunches, our team embraces its members’  deep diversity as a strength and we encourage everyone to bring their whole selves to the workplace. Our dress code is that there is no dress code. Decentral hires based on technical talent and will happily accommodate candidates taking part in any aspect of the selection process: just let us know what you need.</p>                   
