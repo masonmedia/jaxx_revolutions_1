a44baec394786bce82e4c097282597aaef1ae08e
Andrew Mason <andrewmasonmedia@gmail.com>
2018-09-08 13:28:03 -0400

    Added jquery smooth scroll outside of single doc ready function at end of js file -- works for legal page links

--- js/main.js
+++ js/main.js
@@ -510,9 +510,48 @@ $(".content-tween").each(function() {
 //    document.querySelector('#download .btn').addEventListener('mouseover', e => explode(e.pageX, e.pageY));
      
     
-//end doc ready
+//end all doc ready
 });
 
 
+//smooth scroll --> https://css-tricks.com/snippets/jquery/smooth-scrolling/
 
+$(document).ready(function() {
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
+});//end doc ready
